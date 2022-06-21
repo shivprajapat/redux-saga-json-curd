20e3ec58db76986f300f91bba3897a6416177837
yudizshiv <104143801+yudizshiv@users.noreply.github.com>
2022-06-21 22:47:26 +0530

    form component dynamics using state hook

--- src/Pages/AddEditUser.jsx
+++ src/Pages/AddEditUser.jsx
@@ -1,8 +1,8 @@
 import { MDBBtn, MDBValidation } from 'mdb-react-ui-kit';
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState ,useEffect} from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 import TextInput from '../components/TextInput';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createUserStart } from '../redux/actions';
 import { toast } from 'react-toastify';
 const initialState = {
@@ -12,16 +12,31 @@ const initialState = {
   address: ""
 }
 const AddEditUser = () => {
+  const { users } = useSelector((state) => state.data)
+
   const [formValue, setFormValue] = useState(initialState);
+  const [editMode, setEditMode] = useState(false);
+
   const { name, email, phone, address } = formValue;
-  let dispatch = useDispatch()
+  let dispatch = useDispatch();
+  const { id } = useParams()
   let navigate = useNavigate();
+useEffect(() => {
+  if (id) {
+    setEditMode(true);
+    const singleUser = users.find((item)=> item.id !== Number(id));
+    setFormValue({...singleUser})
+  }
+  // eslint-disable-next-line
+}, [id])
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name && email && phone && address) {
       dispatch(createUserStart(formValue))
       toast.success('user Added Successfully');
-       setTimeout(() => {navigate('/')}, 500);
+      setTimeout(() => { navigate('/') }, 500);
     }
   }
   const onInputChange = (e) => {
@@ -35,13 +50,13 @@ const AddEditUser = () => {
         <div className="row">
           <div className="col-lg-5 col-md-5 mx-auto shadow p-3 rounded-5">
             <MDBValidation className='row g-3' noValidate onSubmit={handleSubmit}>
-              <p className='fs-2 fw-bold'>Add user Details</p>
-              <TextInput type='text' name='name' value={name} onChange={onInputChange} invalid='Please enter a valid name' placeholder='Name' />
-              <TextInput type='email' name='email' value={email} onChange={onInputChange} invalid='Please enter a valid email' placeholder='Email' />
-              <TextInput type='phone' name='phone' value={phone} onChange={onInputChange} invalid='Please enter a valid phone' placeholder='Phone' maxLength={10}/>
-              <TextInput type='text' name='address' value={address} onChange={onInputChange} invalid='Please enter a valid address' placeholder='Address' />
+              <p className='fs-2 fw-bold'>{!editMode ? 'Add User Details':"Update User Details"}</p>
+              <TextInput type='text' name='name' value={name || ""} onChange={onInputChange} invalid='Please enter a valid name' placeholder='Name' />
+              <TextInput type='email' name='email' value={email || ""} onChange={onInputChange} invalid='Please enter a valid email' placeholder='Email' />
+              <TextInput type='phone' name='phone' value={phone || ""} onChange={onInputChange} invalid='Please enter a valid phone' placeholder='Phone' maxLength={10} />
+              <TextInput type='text' name='address' value={address || ""} onChange={onInputChange} invalid='Please enter a valid address' placeholder='Address' />
               <div className="col-12 mt-5">
-                <MDBBtn style={{ marginRight: 10 }} type='submit'>Add</MDBBtn>
+                <MDBBtn style={{ marginRight: 10 }} type='submit'>{!editMode ?"Add":"Update"}</MDBBtn>
                 <MDBBtn onClick={() => navigate("/")} color='danger'>Go Back</MDBBtn>
               </div>
             </MDBValidation>
