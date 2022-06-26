11d18e6fd392457cc52cf3950013d4c1f5af4c9f
shivprajapat <shivkumarprajapat232@gmail.com>
2022-06-26 11:25:27 +0530

    chnage server local to heroku server

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const loadUsersApi = async () => await axios.get('http://localhost:5000/users');
-export const createUsersApi = async (user) => await axios.post('http://localhost:5000/users', user);
-export const deleteUsersApi = async (userId) => await axios.delete(`http://localhost:5000/users/${userId}`);
-export const updateUsersApi = async (userId, userInfo) => await axios.put(`http://localhost:5000/users/${userId}`, userInfo);
+export const loadUsersApi = async () => await axios.get('https://redux-saga-curd.herokuapp.com/users/api');
+export const createUsersApi = async (user) => await axios.post('https://redux-saga-curd.herokuapp.com/users/api', user);
+export const deleteUsersApi = async (userId) => await axios.delete(`https://redux-saga-curd.herokuapp.com/users/api/${userId}`);
+export const updateUsersApi = async (userId, userInfo) => await axios.put(`https://redux-saga-curd.herokuapp.com/users/api/${userId}`, userInfo);
